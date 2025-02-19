a32d04c65257c0120bf1ba5370675a05eb2d95ea
Alexandru <alexandru.spinu@endava.com>
2025-02-19 16:04:14 +0200

    Adăugat validare în script.py și workflow

--- script.py
+++ script.py
@@ -1,1 +1,9 @@
-print("Hello, GitHub Actions! Scriptul s-a rulat cu succes!")
+print("testul a rulat")
+
+def main():
+    message = "Hello, GitHub Actions!"
+    print(message)
+    return message
+
+if __name__ == "__main__":
+    assert main() == "Hello, GitHub Actions!", "Test eșuat!"
