b9b5d5f48688ac7eb696f94f9b3025a5a4fdf58d
AlexandruSp <136688321+Al14Spn@users.noreply.github.com>
2025-02-20 10:46:22 +0200

    Update script.py

--- script.py
+++ script.py
@@ -1,9 +1,11 @@
-print("testul a rulat")
+import requests
 
-def main():
-    message = "Hello, GitHub Actions!"
-    print(message)
-    return message
+account_number = "YOUR_accountNumber_PARAMETER"
+url = "https://try.access.worldpay.com/accounts/balance/" + account_number
 
-if __name__ == "__main__":
-    assert main() == "Hello, GitHub Actions!", "Test eșuat!"
+headers = {"WP-CorrelationId": "15cd16b2-7b82-41cb-9b11-21be9dacad88"}
+
+response = requests.get(url, headers=headers, auth=('<username>','<password>'))
+
+data = response.json()
+print(data)
